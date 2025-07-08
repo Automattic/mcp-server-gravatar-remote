52fae45c10674e311e13c65b5e59e509ae1189be
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-07-08 14:58:13 -0500

    Update output schemas

--- src/generated/schemas/interests-output-schema.ts
+++ src/generated/schemas/interests-output-schema.ts
@@ -6,9 +6,9 @@ import { z } from "zod";
  */
 export const interestsOutputSchema = z.object({
   inferredInterests: z.array(z.object({
-  id: z.number(),
-  name: z.string()
-}))
+  id: z.number().describe("The unique identifier for the interest."),
+  name: z.string().describe("The name of the interest as originally defined (most often in English).")
+})).describe("A list of AI-inferred interests")
 });
 
 export type InterestsOutputSchemaType = z.infer<typeof interestsOutputSchema>;
