8da12383e072c4d45a74a4a933260a9efc95419e
Andrew Montgomery <andrewdmontgomery@mac.com>
2025-07-25 16:20:48 -0500

    Add skeleton of auth.ts

---
+++ src/types.ts
@@ -0,0 +1,23 @@
+export interface WordPressUser {
+  ID: number;
+  login: string;
+  email: string;
+  display_name: string;
+  username: string;
+  avatar_URL: string;
+  profile_URL: string;
+  site_count: number;
+  verified: boolean;
+}
+
+export interface WordPressTokenSet {
+  access_token: string;
+  refresh_token?: string;
+  expires_in?: number;
+  token_type: string;
+}
+
+export interface UserProps {
+  claims: WordPressUser;
+  tokenSet: WordPressTokenSet;
+}
